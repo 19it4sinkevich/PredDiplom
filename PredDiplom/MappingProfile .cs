d47f6cea8d18cf7e6bd1d65382c49c3f7b0e22cf
Sinkevich Daniil <19it4.sinkevich.d@pdu.by>
2023-04-13 01:20:13 +0300

    Добавлены объекты передачи данных и маппер

---
+++ PredDiplom/MappingProfile .cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Entities.DataTransferObjects;
+using Entities.Models;
+namespace PredDiplom
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookDto>();
+            CreateMap<BookForCreationDto, Book>();
+            CreateMap<BookForUpdateDto, Book>();
+            CreateMap<BookForUpdateDto, Book>().ReverseMap();
+
+        }
+    }
+
+}
