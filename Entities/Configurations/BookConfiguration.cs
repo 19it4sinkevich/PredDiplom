d9f9ff45c06bdb3f043f033096754500cff2e65f
Sinkevich Daniil <19it4.sinkevich.d@pdu.by>
2023-04-13 01:07:42 +0300

    Добавил модели и их конфигурации

---
+++ Entities/Configurations/BookConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entities.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+            new Book
+            {
+                Id = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"),
+                IBAN = "1723323947",
+                Name = "Пикник на обочине",
+                Genre = "Фантастика",
+                Athor = "Борис Стругатский, Аркадий Стругатский",
+                Description = "«Пикни́к на обо́чине» — философская фантастическая повесть братьев Стругацких, " +
+                "впервые изданная в 1972 году.",
+                TimeOfIssue = "22.03.2023",
+                TimeOfReturn = "14.04.2023"
+            },
+            new Book
+            {
+                Id = new Guid("c9d4c911-49b6-410c-bc78-2d54a9991870"),
+                IBAN = "786324345",
+                Name = "Град обреченный",
+                Genre = "Роман",
+                Athor = "Борис Стругатский, Аркадий Стругатский",
+                Description = "««Град обрече́нный» — философский роман Аркадия и Бориса Стругацких." +
+                " Писался «в стол», когда братья оказались в состоянии мировоззренческого кризиса," +
+                " а затем были резко ограничены в возможностях публиковаться. ",
+                TimeOfIssue = "22.03.2023",
+                TimeOfReturn = "14.04.2023"
+            }) ;
+        }
+    }
+
+}
---
+++ Entities/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Entities.Models
+{
+    public class Book
+    {
+        [Column("BookId")]
+        public Guid Id { get; set; }
+        public string IBAN { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Athor { get; set; }
+        public string Description { get; set; }
+        public string TimeOfIssue { get; set; }
+        public string TimeOfReturn { get; set; }
+
+    }
+
+}
